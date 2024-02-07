0d7dc08a55f973d2efc5c7bed3be2dfb86cd1b15
Pavel Ševčík <pavel.sevcik@protonmail.com>
2024-02-07 03:25:59 +0100

    Add config.server.ts to centralize config handling

--- app/estimator.server.ts
+++ app/estimator.server.ts
@@ -1,21 +1,14 @@
-import * as assert from "assert";
 import { FeeEstimator } from "@samouraiwallet/one-dollar-fee-estimator";
-import { configDotenv } from "dotenv";
 
-const config = configDotenv();
-
-assert.ok(config.parsed?.BITCOIND_USERNAME, "Missing BITCOIND_USERNAME env variable");
-assert.ok(config.parsed?.BITCOIND_PASSWORD, "Missing BITCOIND_PASSWORD env variable");
-assert.ok(config.parsed?.BITCOIND_HOST, "Missing BITCOIND_HOST env variable");
-assert.ok(config.parsed?.BITCOIND_PORT, "Missing BITCOIND_PORT env variable");
+import { serverConfig } from "~/config.server";
 
 export const EstimatorService = new FeeEstimator({
   refresh: 20,
   useWorker: true,
   rpcOptions: {
-    username: config.parsed.BITCOIND_USERNAME,
-    password: config.parsed.BITCOIND_PASSWORD,
-    host: config.parsed.BITCOIND_HOST,
-    port: Number(config.parsed.BITCOIND_PORT),
+    username: serverConfig.BITCOIND_USERNAME,
+    password: serverConfig.BITCOIND_PASSWORD,
+    host: serverConfig.BITCOIND_HOST,
+    port: serverConfig.BITCOIND_PORT,
   },
 });
